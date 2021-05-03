4f8998545fef701a4ac61ecb43454286c2d87c46
vcu-bhattir <78177559+vcu-bhattir@users.noreply.github.com>
2021-05-03 19:41:25 -0400

    Update homework.py

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,23 @@
 def spiralize(number):
-    return_value = 1
+    
+    squaresList = [i*i for i in range(1, number+1) if (i % 2 != 0)]
+    nums = [i for i in range(1, number*number+1) if (i % 2 != 0)]
 
-    return return_value
+    index = 0
+    b = 0
+    totalSum = 0
+
+    for i in nums:
+
+        totalSum += nums[index]
+
+        if (nums[index] == nums[len(nums)-1]):
+            break
+
+        if (nums[index] in squaresList):
+            b += 1
+
+        index += b 
+
+    return totalSum
+
